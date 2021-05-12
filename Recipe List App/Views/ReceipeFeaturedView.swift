1d11e748810b007936fb0136122ac036b180022f
awesomearjun <iamawesomearjun@gmail.com>
2021-05-12 12:10:06 -0400

    • Add RecipeFeaturedView • Modify data flow using environmentobject modifier • Create RecipeModel instance in the RecipeTabView

---
+++ Recipe List App/Views/ReceipeFeaturedView.swift
@@ -0,0 +1,22 @@
+//
+//  ReceipeFeaturedView.swift
+//  Recipe List App
+//
+//  Created by A P on 2021-05-12.
+//
+
+import SwiftUI
+
+struct ReceipeFeaturedView: View {
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ReceipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceipeFeaturedView()
+    }
+}
