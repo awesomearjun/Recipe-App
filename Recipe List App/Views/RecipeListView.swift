201c0d24a8573dbd1b461a29374e29c92252f811
awesomearjun <iamawesomearjun@gmail.com>
2021-05-13 22:00:44 -0400

    • Change SwiftUI list Scrollview, LazyVStack and ForEach • Hide navigationbartitle and use text elect for the title in RecipeListView

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -13,21 +13,35 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List(model.recipes) { r in
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                        HStack {
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50, alignment: .center)
-                                .clipped()
-                                .cornerRadius(5.0)
-                            Text(r.name)
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 40)
+                
+                ScrollView {
+                    LazyVStack (alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    HStack {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5.0)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                })
                         }
-                    })
-            }.navigationBarTitle("Recipes")
+                    }
+                }
+                .navigationBarHidden(true)
+            }
+            .padding(.leading)
         }
     }
 }
@@ -35,6 +49,6 @@ struct RecipeListView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
-            .previewDevice("iPhone 12 Pro Max")
+            .environmentObject(RecipeModel())
     }
 }
