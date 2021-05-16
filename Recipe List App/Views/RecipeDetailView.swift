8ae7dbaa912d44b1606b522eee623bfa2dfa026f
awesomearjun <iamawesomearjun@gmail.com>
2021-05-16 12:48:37 -0400

    • Calculate Ingredient Portions • Add suffix at the end of unit

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     var recipe:Recipe
+    @State var selectedServingSize = 2
     
     
     var body: some View {
@@ -21,6 +22,21 @@ struct RecipeDetailView: View {
                     .resizable()
                     .scaledToFill()
                 
+                // MARK: Serving Size Picker
+                VStack(alignment: .leading) {
+                    Text("Select your serving size:")
+                    
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Spacer()
+                
                 // MARK: Ingredients
                 
                 VStack(alignment: .leading) {
@@ -29,7 +45,7 @@ struct RecipeDetailView: View {
                         .padding([.bottom, .top], 5)
                     
                     ForEach(recipe.ingredients) { item in
-                        Text("• \(item.name)")
+                        Text("• \(RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServing: selectedServingSize)) \(item.name.lowercased())")
                             .font(.body)
                             .padding(.bottom, -0.32)
                     }
